b7365ddf6ad3c382bbf29e6a4da4380f8f5fbcfa
rootVertzon <treinamento@vertzon.com.br>
2025-09-03 21:14:52 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/nelioalves/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,16 @@
+package com.nelioalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nelioalves.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
+
+
+/*esse codigo faz a funçoes findAll(), findById(), save(), delete() sem escrever código, 
+ * pois, JpaRepository é uma interface do Spring Data JPA que já 
+ * traz um monte de métodos prontos para manipular dados no banco*/
---
+++ src/main/java/com/nelioalves/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,16 @@
+package com.nelioalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nelioalves.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
+
+
+/*esse codigo faz a funçoes findAll(), findById(), save(), delete() sem escrever código, 
+ * pois, JpaRepository é uma interface do Spring Data JPA que já 
+ * traz um monte de métodos prontos para manipular dados no banco*/
