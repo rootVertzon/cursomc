d96af0adf836a410d7189a3cef42a038cc4b6641
rootVertzon <treinamento@vertzon.com.br>
2025-10-26 20:51:41 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,26 @@
+package com.nelioalves.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 3635965500290894225L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer stattus, String msg, Long timeStamp) {
+		super(stattus, msg, timeStamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+	
+
+}
